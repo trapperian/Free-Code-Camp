2ac09cf1154ca57bb1bc51678ea6f2acbf8e8822
Ian <44474914+trapperian@users.noreply.github.com>
2022-09-18 12:10:56 -0600

    Create create-strings-using-template-literals.js

---
+++ create-strings-using-template-literals.js
@@ -0,0 +1,18 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let i = 0; i < result.failure.length; i++) {
+const liString = `<li class="text-warning">${result.failure[i]}</li>`;
+failureItems.push(liString);
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
