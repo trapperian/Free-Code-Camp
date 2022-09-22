ab880d20ae3f53037f746ea29bb8170d85fde4a8
Ian <44474914+trapperian@users.noreply.github.com>
2022-09-22 11:55:55 -0600

    Create string_arranger function

--- Scientific Computing Python/arithmetic_arranger.py
+++ Scientific Computing Python/arithmetic_arranger.py
@@ -1,15 +1,15 @@
 # arithmetic arranger.  This takes math strings horizontal and returns them in vertical
 import re
 
-
-def arranger(input):
-    if type(input) != list:
+# this function converts strings to integers and does the arithmetic in the event show_answers=True
+def do_the_math(input_list):
+    if type(input_list) != list:
         return print("Please use a list data type.")
-    if len(input) > 5:
+    if len(input_list) > 5:
         return print(
             "We can only take a maximum of 5 problems at a time, please use fewer."
         )
-    for i in input:
+    for i in input_list:
         result = re.search(r"[+,\-,\*,\/]", i)
 
         try:
@@ -21,24 +21,44 @@ def arranger(input):
             item = i.split(operation)
             total = 0
             for x in item:
-                total += int(x)
-            print("The sum of " + i + " is " + str(total))
+                return total += int(x)
+            # print("The sum of " + i + " is " + str(total))
         elif operation == "-":
             item = i.split(operation)
             item = list(map(int, item))
-            difference = item[0] - item[1]
-            print("The difference of " + i + " is " + str(difference))
+            return difference = item[0] - item[1]
+            # print("The difference of " + i + " is " + str(difference))
         elif operation == "*":
             item = i.split(operation)
             item = list(map(int, item))
-            product = item[0] * item[1]
-            print("The product of " + i + " is " + str(product))
+            return product = item[0] * item[1]
+            # print("The product of " + i + " is " + str(product))
         elif operation == "/":
             item = i.split(operation)
             item = list(map(int, item))
-            division = item[0] / item[1]
-            print("The division of " + i + " is " + str(division))
+            return division = item[0] / item[1]
+            # print("The division of " + i + " is " + str(division))
 
+# this function will actually print the arranged strings
+def string_manipulation(input_list):
+    for i in input_list:
+        result = re.search(r"[+,\-,\*,\/]", i)
 
+        try:
+            operation = result.group()
+        except:
+            print("There is no proper operator in " + i)
+            continue
+        if operation == "+":
+            item = i.split(operation)
+
+        elif operation == "-":
+            item = i.split(operation)
+
+        elif operation == "*":
+            item = i.split(operation)
+
+        elif operation == "/":
+            item = i.split(operation)
 # test case
-arranger(["5 % 6", "1 - 3", "1 + 2", "6 / 3", "13 * 8"])
+
